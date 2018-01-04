7dc34d35464de9b0600a3075b081488e19855dc1
Alex Pygin <alexpygin@gmail.com>
2018-01-04 09:25:44 -0800

    Fixing potential problems

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -4,12 +4,12 @@ import 'rxjs/add/operator/map';
 
 @Injectable()
 export class ProductService {
+  
+  private _albumUrl = "../assets/album.json";
 
   constructor(private _http: Http) { }
-  
-  _albumUrl: string = "../assets/album.json";
 
   private getAlbum(id: number) {
-    return this._http.get(this._albumUrl).map(response => response.json());
+    return this._http.get(this._albumUrl).map((response) => response.json());
   }
 }
